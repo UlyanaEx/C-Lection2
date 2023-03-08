a278d421c0fde64922c6b4139dc6b97f03e4aab6
«Ulyana <eksterua@gmail.com>
2023-03-08 09:12:53 +0300

    Example010 Find Index

---
+++ C#/Lection2/Example009_Findindex/Program.cs
@@ -0,0 +1,25 @@
+﻿// у нас есть массив array, в котором n элементов. Найдём элемент, совпадающий снекоторым значением, который определяет пользователь
+
+int[] array = { 11, 22, 80, 4, 57, 68, 76, 80 };
+int n = array.Length;
+int find = 80;
+int index = 0;
+
+while (index < n)
+{
+   if (array[index] == find)
+{
+    Console.WriteLine(index);
+    break;
+    }
+      index++;
+}
+
+
+
+
+
+
+
+
+
