c9705352ebc1a6458256e2325b98d3412e14f578
«Ulyana <eksterua@gmail.com>
2023-03-08 08:06:56 +0300

    Example008 Add Function Max

--- C#/Lection2/Example_008/Program.cs
+++ C#/Lection2/Example_008/Program.cs
@@ -1,7 +1,15 @@
 ﻿// Ищем максимум из 9 чисел
 
+int Max (int Arg1, int Arg2, int Arg3)
+{
+    int result = Arg1;
+    if (Arg2 > result) result = Arg2;
+    if (Arg3 > result) result = Arg3; 
+    return result;
+}
+
 int a1 = 7;
-int b1 = 3;
+int b1 = 13;
 int c1 = 9;
 
 int a2 = 110;
@@ -10,16 +18,11 @@ int c2 = 4;
 
 int a3 = 25;
 int b3 = 0;
-int c3 = 8;
+int c3 = 800;
 
-int max = a1;
-if (b1 > max) max = b1;
-if (c1 > max) max = c1;
-if (a2 > max) max = a2;
-if (b2 > max) max = b2;
-if (c2 > max) max = c2;
-if (a3 > max) max = a3;
-if (b3 > max) max = b3;
-if (c3 > max) max = c3;
+int Max1 = Max (a1, b1, c1);
+int Max2 = Max (a2, b2, c2);
+int Max3 = Max (a3, b3, c3);
+int MaxResult = Max (Max1, Max2, Max3);
 
-Console.WriteLine(max);
+Console.WriteLine(MaxResult);
