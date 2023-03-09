9dd82a195401e3ea5a90cdf0f241f21b1ef1a036
«Ulyana <eksterua@gmail.com>
2023-03-09 13:53:50 +0300

    Task3 HomeWork

---
+++ C#/Practice2/HomeWork/Task3_HomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+void ThirdDigit(int num)
+{
+    Console.WriteLine(num);
+    if (num < 100)
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+    else
+    {
+        while (num > 999)
+        {
+            num = num / 10;
+        }
+        Console.WriteLine(num % 10);
+    }
+}
+
+ThirdDigit(new Random().Next());
+
+
