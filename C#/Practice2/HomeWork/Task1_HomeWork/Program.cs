e9860b8982ece7576f933628d38d38b0c29fa942
«Ulyana <eksterua@gmail.com>
2023-03-09 13:15:04 +0300

    Task1 HomeWork

---
+++ C#/Practice2/HomeWork/Task1_HomeWork/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+int FindSecondDigit(int num)
+{
+    Console.WriteLine(num);
+    int a = num / 10;
+    a = a % 10;
+    return a;
+}
+
+int b = FindSecondDigit(new Random().Next(100,1000));
+Console.WriteLine(b);
+
+
