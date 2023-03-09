8c2920b8273316551aaf48306a44f985bdaacb9e
«Ulyana <eksterua@gmail.com>
2023-03-09 14:21:26 +0300

    Task4 HomeWork

---
+++ C#/Practice2/HomeWork/Task4_HomeWork/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+void Day(int num)
+{
+    Console.WriteLine(num);
+    if ((num == 6) | (num == 7))
+    {
+        Console.WriteLine("Это выходной");
+    }
+    else if (num > 0 & num < 6)
+    {
+        Console.WriteLine("Это рабочий день");
+    }
+    else 
+    {
+        Console.WriteLine("Некорректные данные");
+    }
+}
+Console.WriteLine("Введите число от 1 до 7");
+string s_a = Console.ReadLine()!;
+int a = int.Parse(s_a);
+Day(a);
+
+
